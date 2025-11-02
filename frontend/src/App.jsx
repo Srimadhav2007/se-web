c3adc0a72a53a524a2fc5b44f25a9540bc1b89b0
Srimadhav2007 <phanisrimadhav.mula@gmail.com>
2025-11-02 16:23:06 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import background from './assets/video.mp4'
+
+function App() {
+
+  return (
+    <>
+      <video src={background} muted autoPlay loop></video>
+      <h1>कालरूपाय नमः शिवाय||</h1>
+    </>
+  )
+}
+
+export default App
